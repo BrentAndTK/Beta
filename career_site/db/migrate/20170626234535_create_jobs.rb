77049917014cb27bbc81f4c4ffff0b9b1647799a
Brent Mennen <brentmennen@Brents-MacBook-Pro.local>
2017-06-26 18:52:38 -0500

    Create scaffold for jobs
      $ rails generate scaffold job title:string description:string
      salary:decimal manager:belongs_to

---
+++ career_site/db/migrate/20170626234535_create_jobs.rb
@@ -0,0 +1,12 @@
+class CreateJobs < ActiveRecord::Migration[5.1]
+  def change
+    create_table :jobs do |t|
+      t.string :title
+      t.string :description
+      t.decimal :salary
+      t.belongs_to :manager, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
