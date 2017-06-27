2a6ac792f039c8fa35d31c94e5982d4a872e85b5
Brent Mennen <brentmennen@Brents-MacBook-Pro.local>
2017-06-27 12:46:31 -0500

    Add login/logout to sessions controller and add routes

--- career_site/app/controllers/sessions_controller.rb
+++ career_site/app/controllers/sessions_controller.rb
@@ -1,11 +1,16 @@
 class SessionsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
+  def login
+  end
+
   def create
     auth_hash= request.env['omniauth.auth']
     @user = User.find_or_create_by(uid: auth_hash['uid']) do |user|
       user.name = auth_hash['info']['name']
       user.nickname = auth_hash['info']['nickname']
     end
+  end
+  def logout
   end
 end
--- career_site/config/routes.rb
+++ career_site/config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   resources :jobs
   resources :managers
   match 'auth/:provider/callback', to: 'sessions#create', via: [:post,:get]
+  get 'login', to: 'sessions#login', as: 'login'
+  get 'logout', to: 'sessions#login', as: 'logout'
+  get 'auth/github', as: 'github_login'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
