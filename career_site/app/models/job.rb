14d9620b74cc0a8a5b43fdbc8c0f9780b044a6a8
Tykiera <tolivest@hawk.iit.edu>
2017-06-27 16:37:17 -0500

    Add validations

--- career_site/app/models/job.rb
+++ career_site/app/models/job.rb
@@ -1,6 +1,13 @@
 class Job < ApplicationRecord
 
-  validates title: description: 
+  validates :title, :description, :salary,  presence:true
+
+  validates :salary, format: {
+    with: ^(((\d{1,3})(,\d{3})*)|(\d+)(\.\d{2})?$
+    message: "Must be a valid number"
+    }
+
+  validates :description
 
   belongs_to :manager
   belongs_to :manager, through: :activejobs
