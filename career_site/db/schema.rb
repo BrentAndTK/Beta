8f9390735a8b070b769d418ae56fc94ff6555720
Brent Mennen <brentmennen@Brents-MacBook-Pro.local>
2017-06-27 12:11:14 -0500

    Run migration
      $rake db:migrate

--- career_site/db/schema.rb
+++ career_site/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170627010450) do
+ActiveRecord::Schema.define(version: 20170627170746) do
 
   create_table "activejobs", force: :cascade do |t|
     t.integer "manager_id"
@@ -32,6 +32,14 @@ ActiveRecord::Schema.define(version: 20170627010450) do
   create_table "managers", force: :cascade do |t|
     t.string "name"
     t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "uid"
+    t.string "name"
+    t.string "nickname"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
