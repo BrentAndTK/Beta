fe111b90b3c169ba137717d41f39b3501f9130ca
Tykiera <tolivest@hawk.iit.edu>
2017-06-26 23:44:12 -0500

    Add testing error methods 	test  " .. " do

--- career_site/test/models/job_test.rb
+++ career_site/test/models/job_test.rb
@@ -1,6 +1,15 @@
 require 'test_helper'
 
 class JobTest < ActiveSupport::TestCase
+
+ job = Job.new
+ job.invalid?
+test "job requires all fields"
+ assert job.errors[:title].any?
+ assert job.errors[:description].any?
+ assert job.errors[:salary].any?
+
+
   # test "the truth" do
   #   assert true
   # end
