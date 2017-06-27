af1f943678df075e17fcfa0d58ad89ecebc2b8be
Brent Mennen <brentmennen@Brents-MacBook-Pro.local>
2017-06-27 17:22:04 -0500

    Fix routing for logout

--- career_site/config/routes.rb
+++ career_site/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :managers
   match 'auth/:provider/callback', to: 'sessions#create', via: [:post,:get]
   get 'login', to: 'sessions#login', as: 'login'
-  get 'logout', to: 'sessions#login', as: 'logout'
+  get 'logout', to: 'sessions#logout', as: 'logout'
   get 'auth/github', as: 'github_login'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
