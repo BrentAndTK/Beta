68da03897c1beed3de50a7521cc47cd7351ea358
Tykiera <tolivest@hawk.iit.edu>
2017-06-28 15:29:17 -0500

    Add seed data

--- career_site/db/seeds.rb
+++ career_site/db/seeds.rb
@@ -16,3 +16,23 @@ description:
 },
 salary: 30000.00
 )
+
+Jobs.create!(
+title: 'Ipsum Lorem',
+description:
+%{<p>
+  <em> Ipsum Dolor sit amet </em>
+  Morbi eleifend a sem ac ultricies. Curabitur fermentum rhoncus velit, ac pulvinar erat facilisis mattis. Pellentesque convallis mi lacus, ut vulputate neque egestas faucibus. Nullam quis venenatis leo, finibus ultrices nunc. Duis non purus risus. Ut sit amet tellus dignissim, ornare mauris in, pharetra velit. Proin vestibulum ex a turpis commodo gravida. </p>
+},
+salary: 60000.00
+)
+
+Jobs.create!(
+title: 'Sit Amet',
+description:
+%{<p>
+  <em> Dolor sit amet </em>
+  Morbi eleifend a sem ac ultricies. Curabitur fermentum rhoncus velit, ac pulvinar erat facilisis mattis. Pellentesque convallis mi lacus, ut vulputate neque egestas faucibus. Nullam quis venenatis leo, finibus ultrices nunc. Duis non purus risus. Ut sit amet tellus dignissim, ornare mauris in, pharetra velit. Proin vestibulum ex a turpis commodo gravida. </p>
+},
+salary: 12.00
+)
