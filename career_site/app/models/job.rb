23e837bac35d4fac7975c9dead798f3ad5934fa5
Brent Mennen <brentmennen@Brents-MacBook-Pro.local>
2017-06-28 22:27:27 -0500

    Commented out salary validation - comming back with a syntax error

--- career_site/app/models/job.rb
+++ career_site/app/models/job.rb
@@ -1,11 +1,12 @@
 class Job < ApplicationRecord
-<<<<<<< HEAD
   belongs_to :user
   has_many :managers
 
   validates :title, :description, :salary,  presence:true
+=begin
   validates :salary, format: {
     with: (/[0-9]{0,3}[\,]?[0-9]{0,3}[\.]?[0-9]{2}/)
     message: "Must be a valid number"
-    }
+  }
+=end
 end
